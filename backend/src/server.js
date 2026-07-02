fb41caa17a3a8b03f5fc558c9273c8363aef11f2
Soumya Priyadarshini <soumyapriyadarshini41@gmail.com>
2026-07-03 01:42:27 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import notesRoutes from './routes/notesRoutes.js';
+import {connectDB} from './config/db.js';
+import rateLimiter from './middleware/rateLimiter.js';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+   origin: "http://localhost:5173",// Replace with your frontend URL
+})
+); // Enable CORS for all routes
+
+// Middleware to parse JSON bodies: req.body
+app.use(express.json());
+app.use(rateLimiter); // Apply the rate limiter middleware to all routes
+
+
+// our simple custom middleware to log the request method and url
+// app.use((req,res,next)=>{
+//   console.log(`Req method is ${req.method} & Req url is ${req.url}`);
+//   next();
+// });
+
+app.use("/api/notes", notesRoutes);
+
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+  console.log('Server is running on port:',PORT);
+   })
+});
+
+
